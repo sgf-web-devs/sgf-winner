6f24056e288502c0114b1eeef2c88fac977c4079
Myke Bates <mbates@thealchemediaproject.com>
2016-03-01 14:34:11 -0600

    core pusher implementation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('github_id')->unique();
+            $table->string('email');
+            $table->string('picture');
             $table->timestamps();
         });
     }
