6b01917f41175f32bec21586ba368f9c720fbd44
Myke Bates <mbates@thealchemediaproject.com>
2016-03-02 02:14:57 -0600

    all sorts

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Events\DevCheckedIn;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Validator;
 use Illuminate\Cookie\CookieJar;
@@ -15,75 +16,36 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    /**
-     * Responds to requests to GET /users
-     */
     public function getIndex()
     {
-//        //return Auth::user()->email;
-//
-//
-//        event(new DevCheckedIn(Auth::user()));
-//
-//
-//        $response = new Response(view('users')->with(['data'=>Auth::user()]));
-//        if (Cookie::get('success') !== false) {
-//            $response->withCookie(Cookie::forget('success'));
-//            return $response;
-//        }
-//
-//        return $response;
-////        return view("users");
+        //$time = Carbon::now()->addSeconds(3);
+        $time = new Carbon('first wednesday of this month 6:30PM CST');
+
+        if(Carbon::now() > $time) {
+            $time = new Carbon('first wednesday of next month 6:30PM CST');
+        }
 
         $users = User::All();
 
-        return view("userlist")->with(array('users' => $users));
+        $data = [
+            'users' => $users,
+            'time' => $time,
+            'quip' => '6RaPf0qc',
+        ];
 
+        return view("userlist", $data);
     }
 
-    /**
-     * Responds to requests to POST
-     * @param Request $request
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-//    public function postIndex(Request $request)
-//    {
-////        $post_data = Input::all();
-//        $new_user = new User;
-//        $validator = Validator::make($request->all(), [
-//            'name' => 'required',
-//            'email' => 'required|unique:users',
-//        ]);
-//
-//        if ($validator->fails()) {
-//            return redirect('/')
-//                ->withErrors($validator)
-//                ->withInput();
-//        }
-//        $new_user->name = $request->name;
-//        $new_user->email = $request->email;
-//
-//        $new_user->save();
-//
-//        $response = new Response(redirect('/'));
-//
-//        $response->withCookie(cookie('entrysuccess', true, 3600));
-//
-//        return $response;
-////        return view("users")->withCookie(cookie('success', '1', 60));
-////            ;
-//
-//    }
-
-
-    /**
-     * Responds to requests to POST
-     */
     public function getWinners()
     {
         $winners = User::All()->random(2);
 
         return view("winner")->with(array('users' => $winners));
-        //
+    }
+
+    public function getAll()
+    {
+        $users = User::All();
+        return $users;
     }
 }
--- resources/views/userlist.blade.php
+++ resources/views/userlist.blade.php
@@ -1,103 +1,46 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Signup Sheet</title>
+@extends('layouts/app')
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <script src="https://js.pusher.com/3.0/pusher.min.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
-    <style>
-        html, body {
-            height: 100%;
-        }
+@section('body')
+    <div id="countdown"></div>
 
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 96px;
-        }
-        td {
-            padding: 15px;
-        }
-        .user-list ul {
-            list-style: none;
-            padding-left: 0;
-        }
-        .user-list ul li {
-            display: inline-block;
-            max-width: 33%;
-            padding: 15px;
-        }
-        .user-list ul li .email, .user-list ul li .name {
-            display: block;
-            font-size:20px;
-            font-weight: bold;
-        }
-        .user-list ul li img {
-            max-width: 212px;
-            border-radius: 100px;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <h1>Signup Sheet</h1>
     <div class="user-list">
-        <ul>
+        <ul class="count_{{$users->count()}}">
             @foreach($users as $cur_user)
                 <li>
-                    <img src="{{ $cur_user->picture }}" />
+                    <img src="{{ $cur_user->picture }}"/>
                     <span class="name">{{ $cur_user->name }}</span>
-                    <span class="email">{{ $cur_user -> email }}</span>
                 </li>
             @endforeach
-
         </ul>
     </div>
-</div>
-<script>
-    var pusher = new Pusher('{{ env('PUSHER_KEY') }}', {
-        encrypted: true
-    });
+@stop
 
-    var channel = pusher.subscribe('sgf-channel');
-    channel.bind('DevCheckedIn', function (data) {
-        console.log($('.email'));
-        if ($('.user-list ul li').length !== 0) {
+@section('scripts')
+    <script>
+        var pusher = new Pusher('{{ env('PUSHER_KEY') }}', {
+            encrypted: true
+        });
 
-            $('.email').each(function() {
+        var channel = pusher.subscribe('sgf-channel');
+        channel.bind('DevCheckedIn', function (data) {
+            $.get('/users/all', function(data){
+                $('.user-list ul').html('');
+                $('.user-list ul').removeClass('').addClass('count_' + data.length);
 
-                if ($(this).text() !== data.user.email) {
-                    $('.user-list ul').append('<li><img src=' + data.user.picture + ' /><span class="name">' + data.user.name + '</span><span class="email"> ' + data.user.email + '</span></li>');
-                } else {
-                    return;
-                }
-
+                _.each(data, function (user) {
+                    $('.user-list ul').append('<li><img src=' + user.picture + ' /><span class="name">' + user.name + '</span></li>');
+                });
             });
-        }
-        else {
-            $('.user-list ul').append('<li><img src=' + data.user.picture + ' /><span class="name">' + data.user.name + '</span><span class="email">' + data.user.email + '</span></li>');
-        }
-    });
-</script>
-</body>
-</html>
+        });
 
+
+
+        $("#countdown").countdown('{{ $time }}', function (event) {
+            $(this).text(event.strftime('%H:%M:%S'));
+        }).on('finish.countdown', function () {
+            $('#countdown').after('<div class="fitvid"><iframe width="1280" height="720" src="https://beta.quipvid.com/embed/{{$quip}}" frameborder="0" allowfullscreen></iframe></div>');
+            $(".fitvid").fitVids();
+            $('.fitvid').animateCSS('fadeIn');
+        });
+    </script>
+@stop
